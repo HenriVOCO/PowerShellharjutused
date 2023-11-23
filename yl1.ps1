4189be0192f093fb86bc0ae14aa010e35b794bac
HenriVOCO <henri.lemmats.001@voco.ee>
2023-11-23 15:34:28 +0200

    Lokaalse kasutaja loomine inputi järgi

---
+++ yl1.ps1
@@ -0,0 +1,14 @@
+﻿$eesnimi = Read-Host "Sisesta oma eesnimi"
+$perenimi = Read-Host "Sisesta oma perenimi"
+
+#$kood = Read-Host "Sisesta oma parool"
+#$kasutajaparool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+$kasutajanimi = "$($eesnimi.ToLower()).$($perenimi.ToLower())"
+#New-LocalUser -Name $kasutajanimi
+
+Write-Host "Loodav kasutaja on $kasutajanimi"
+
+$parool = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+
+New-LocalUser -Name "$kasutajanimi" -Password $parool -FullName "$eesnimi $perenimi" -Description "Local Account"
